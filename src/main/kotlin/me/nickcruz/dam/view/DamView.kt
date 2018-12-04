ad7e3b69f28b600d0c5f3f7012f5a9a543cc747e
Nick Cruz <ncruz@umich.edu>
2018-12-04 07:45:21 -0800

    Move view title to companion object.

--- src/main/kotlin/me/nickcruz/dam/view/DamView.kt
+++ src/main/kotlin/me/nickcruz/dam/view/DamView.kt
@@ -13,7 +13,7 @@ import tornadofx.vbox
 /**
  * Initial view for DAM. app.
  */
-class DamView : View("DAM.") {
+class DamView : View(VIEW_TITLE) {
 
     private var rootDirectoryField: TextField by singleAssign()
     private val imageExplorerViewModel = ImageExplorerViewModel()
@@ -25,5 +25,9 @@ class DamView : View("DAM.") {
                 textProperty().onChange { it?.run { imageExplorerViewModel.setRootDirectory(it) } }
             }
         }
+    }
+
+    companion object {
+        private const val VIEW_TITLE = "DAM."
     }
 }
