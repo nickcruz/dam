f1288b4b2159a375e021a20d3b78860a701a825b
Nick Cruz <ncruz@umich.edu>
2018-12-09 15:31:28 -0800

    Load all images in FlowPane.

--- src/main/kotlin/me/nickcruz/dam/view/DamView.kt
+++ src/main/kotlin/me/nickcruz/dam/view/DamView.kt
@@ -3,16 +3,18 @@ package me.nickcruz.dam.view
 import javafx.event.EventHandler
 import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.layout.FlowPane
 import javafx.stage.DirectoryChooser
 import me.nickcruz.dam.viewmodel.ImageExplorerViewModel
 import tornadofx.View
 import tornadofx.button
-import tornadofx.cache
 import tornadofx.fitToParentSize
+import tornadofx.flowpane
 import tornadofx.hbox
 import tornadofx.imageview
 import tornadofx.label
-import tornadofx.listview
+import tornadofx.onChange
+import tornadofx.scrollpane
 import tornadofx.singleAssign
 import tornadofx.vbox
 import java.io.File
@@ -24,6 +26,7 @@ class DamView : View(VIEW_TITLE) {
 
     private var chooseRootDirectoryButton: Button by singleAssign()
     private var rootDirectoryLabel: Label by singleAssign()
+    private var imageGalleryFlowPane: FlowPane by singleAssign()
 
     private val directoryChooser = DirectoryChooser().apply {
         title = CHOOSE_ROOT_DIRECTORY
@@ -37,12 +40,11 @@ class DamView : View(VIEW_TITLE) {
             chooseRootDirectoryButton = button(CHOOSE_ROOT_DIRECTORY)
             rootDirectoryLabel = label()
         }
-        listview(imageExplorerViewModel.images) {
+        scrollpane(true, true) {
             fitToParentSize()
-            cellFormat {
-                graphic = cache {
-                    imageview(it.toURI().toString())
-                }
+            imageGalleryFlowPane = flowpane {
+                hgap = GALLERY_IMAGE_GAP
+                vgap = hgap
             }
         }
     }
@@ -54,11 +56,20 @@ class DamView : View(VIEW_TITLE) {
             }
         }
         imageExplorerViewModel.onRootDirectoryChanged { rootDirectoryLabel.text = it.absolutePath }
+        imageExplorerViewModel.images.onChange {
+            imageGalleryFlowPane.children.setAll(it.list.map { file ->
+                imageview(file.toURI().toString()) {
+                    isPreserveRatio = true
+                    fitHeight = 300.0
+                }
+            })
+        }
     }
 
     companion object {
         private const val VIEW_TITLE = "DAM."
         private const val CHOOSE_ROOT_DIRECTORY = "Choose a root directory"
+        private const val GALLERY_IMAGE_GAP = 4.0
 
         private val INITIAL_DIRECTORY = System.getProperty("user.home")
     }
