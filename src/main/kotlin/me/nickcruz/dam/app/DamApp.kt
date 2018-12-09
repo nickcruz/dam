2b781f3724bdd09e5e18cbec1eca1eac112e5b14
Nick Cruz <ncruz@umich.edu>
2018-12-09 14:54:15 -0800

    Load images from URI and display in listview.

--- src/main/kotlin/me/nickcruz/dam/app/DamApp.kt
+++ src/main/kotlin/me/nickcruz/dam/app/DamApp.kt
@@ -6,8 +6,7 @@ import tornadofx.App
 
 class DamApp : App(DamView::class) {
     override fun start(stage: Stage) {
-        stage.minHeight = 400.0
-        stage.minWidth = 600.0
+        stage.isMaximized = true
         super.start(stage)
     }
 }
--- src/main/kotlin/me/nickcruz/dam/view/DamView.kt
+++ src/main/kotlin/me/nickcruz/dam/view/DamView.kt
@@ -1,13 +1,16 @@
 package me.nickcruz.dam.view
 
 import javafx.event.EventHandler
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.stage.DirectoryChooser
 import me.nickcruz.dam.viewmodel.ImageExplorerViewModel
 import tornadofx.View
 import tornadofx.button
+import tornadofx.cache
 import tornadofx.fitToParentSize
 import tornadofx.hbox
+import tornadofx.imageview
 import tornadofx.label
 import tornadofx.listview
 import tornadofx.singleAssign
@@ -19,6 +22,7 @@ import java.io.File
  */
 class DamView : View(VIEW_TITLE) {
 
+    private var chooseRootDirectoryButton: Button by singleAssign()
     private var rootDirectoryLabel: Label by singleAssign()
 
     private val directoryChooser = DirectoryChooser().apply {
@@ -28,19 +32,28 @@ class DamView : View(VIEW_TITLE) {
 
     private val imageExplorerViewModel = ImageExplorerViewModel()
 
-    init {
-        imageExplorerViewModel.onRootDirectoryChanged { rootDirectoryLabel.text = it.absolutePath }
-    }
-
     override val root = vbox {
-        fitToParentSize()
         hbox {
-            button(CHOOSE_ROOT_DIRECTORY).onAction = EventHandler {
-                imageExplorerViewModel.rootDirectory = directoryChooser.showDialog(currentWindow)
-            }
+            chooseRootDirectoryButton = button(CHOOSE_ROOT_DIRECTORY)
             rootDirectoryLabel = label()
         }
-        listview(imageExplorerViewModel.images)
+        listview(imageExplorerViewModel.images) {
+            fitToParentSize()
+            cellFormat {
+                graphic = cache {
+                    imageview(it.toURI().toString())
+                }
+            }
+        }
+    }
+
+    init {
+        chooseRootDirectoryButton.onAction = EventHandler {
+            directoryChooser.showDialog(currentWindow)?.let { directory ->
+                imageExplorerViewModel.rootDirectory = directory
+            }
+        }
+        imageExplorerViewModel.onRootDirectoryChanged { rootDirectoryLabel.text = it.absolutePath }
     }
 
     companion object {
